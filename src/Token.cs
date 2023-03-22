d907542f651c2a48205c5ca20992fd107214059f
mhumelsine <mhumelsine@gmail.com>
2023-03-22 19:25:42 -0400

    computer blue screened

--- src/Token.cs
+++ src/Token.cs
@@ -2,5 +2,18 @@ namespace Lox;
 
 class Token
 {
-    
+    readonly TokenType type;
+    readonly string lexeme;
+    readonly object literal;
+    readonly int line;
+
+    public Token(TokenType type, string lexeme, object literal, int line)
+    {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    public override string ToString() => $"{type} {lexeme} {literal}";
 }
